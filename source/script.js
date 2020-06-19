5433c8071f23b69dccfa6da6ea49e0bfde7b1e72
Max S. Haberman <max@surveycto.com>
2020-06-19 13:23:03 -0400

    Replaced double-quotes with single-quotes where necessary

--- source/script.js
+++ source/script.js
@@ -1,22 +1,22 @@
 // References to field elements
-var fromNumberField = document.getElementById("fromNumber")
-var toNumberField = document.getElementById("toNumber")
-var dialBtn = document.getElementById("dial")
-var updateStatusBtn = document.getElementById("updateStatus")
-var sidField = document.getElementById("sid")
-var statusField = document.getElementById("status")
+var fromNumberField = document.getElementById('fromNumber')
+var toNumberField = document.getElementById('toNumber')
+var dialBtn = document.getElementById('dial')
+var updateStatusBtn = document.getElementById('updateStatus')
+var sidField = document.getElementById('sid')
+var statusField = document.getElementById('status')
 
 // References to values stored in the plug-in parameters
-var authToken = getPluginParameter("authToken")
-var accountSID = getPluginParameter("accountSID")
-var fromNumber = getPluginParameter("fromNumber")
-var toNumber = getPluginParameter("toNumber")
-var twilioNumber = getPluginParameter("twilioNumber")
-var record = Number(getPluginParameter("record"))
-var displayNumber = Number(getPluginParameter("displayNumber"))
+var authToken = getPluginParameter('authToken')
+var accountSID = getPluginParameter('accountSID')
+var fromNumber = getPluginParameter('fromNumber')
+var toNumber = getPluginParameter('toNumber')
+var twilioNumber = getPluginParameter('twilioNumber')
+var record = Number(getPluginParameter('record'))
+var displayNumber = Number(getPluginParameter('displayNumber'))
 
 //other variables
-var rootUrl = "https://api.twilio.com"
+var rootUrl = 'https://api.twilio.com'
 var callJSON
 
 // Define the dial function
@@ -24,29 +24,30 @@ dialBtn.onclick = function () { dial(); }
 
 updateStatusBtn.onclick = function () { updateStatus(); }
 
-function makeHttpObject() {
+function makeHttpObject () {
   try {
     return new XMLHttpRequest()
-  } catch (error) {}
+  } catch (error) { }
   try {
-    return new ActiveXObject("Msxml2.XMLHTTP")
-  } catch (error) {}
+    return new ActiveXObject('Msxml2.XMLHTTP')
+  } catch (error) { }
   try {
-    return new ActiveXObject("Microsoft.XMLHTTP")
-  } catch (error) {}
+    return new ActiveXObject('Microsoft.XMLHTTP')
+  } catch (error) { }
 
-  throw new Error("Could not create HTTP request object.")
+  throw new Error('Could not create HTTP request object.')
 }
 
-function updateStatusField(value) {
+function updateStatusField (value) {
   statusField.innerHTML = value
 }
 
-function updateStatus() {
+function updateStatus () {
 
   var responseText = getMetaData()
 
-  if (!responseText) {  updateStatusField("The call metadata is undefined")
+  if (!responseText) {
+    updateStatusField('The call metadata is undefined')
 
     return false
   }
@@ -57,18 +58,18 @@ function updateStatus() {
 
   } catch (error) {
 
-    updateStatusField("Response JSON not valid")
+    updateStatusField('Response JSON not valid')
 
     return false
   }
 
   if (!callJSON.uri) {
 
-    updateStatusField("Status uri is undefined")
+    updateStatusField('Status uri is undefined')
 
     return false
   }
-  
+
   var request = undefined
   var requestUrl = rootUrl + callJSON.uri
 
@@ -76,9 +77,9 @@ function updateStatus() {
 
     request = makeHttpObject()
 
-    request.open("POST", requestUrl, true)
-    request.setRequestHeader("Authorization","Basic " + btoa(unescape(encodeURIComponent(accountSID + ":" + authToken))))
-    request.setRequestHeader("Content-type","application/x-www-form-urlencoded")
+    request.open('POST', requestUrl, true)
+    request.setRequestHeader('Authorization', 'Basic ' + btoa(unescape(encodeURIComponent(accountSID + ':' + authToken))))
+    request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded')
 
     request.onreadystatechange = function () {
 
@@ -88,7 +89,7 @@ function updateStatus() {
 
         if (!responseText) {
 
-          updateStatusField("undefined error")
+          updateStatusField('undefined error')
 
         } else {
 
@@ -108,27 +109,27 @@ function updateStatus() {
   }
 }
 
-function dial() {
+function dial () {
 
-  var responseText = ""
+  var responseText = ''
 
   var request = undefined
 
-  var requestUrl = rootUrl + "/2010-04-01/Accounts/" + accountSID + "/Calls.json"
+  var requestUrl = rootUrl + '/2010-04-01/Accounts/' + accountSID + '/Calls.json'
 
-  var Twiml = '<Response><Dial record="' + (record === 0 ? "do-not-record" : "record-from-ringing-dual") + '" answerOnBridge="true" callerId="' + twilioNumber + '">' + toNumber + "</Dial></Response>"
+  var Twiml = '<Response><Dial record="' + (record === 0 ? 'do-not-record' : 'record-from-ringing-dual') + '" answerOnBridge="true" callerId="' + twilioNumber + '">' + toNumber + '</Dial></Response>'
 
-  var params = "From=" + twilioNumber + "&To=" + fromNumber + "&Twiml=" + Twiml
+  var params = 'From=' + twilioNumber + '&To=' + fromNumber + '&Twiml=' + Twiml
 
-  updateStatusField("Call attempted")
+  updateStatusField('Call attempted')
 
   try {
 
     request = makeHttpObject()
 
-    request.open("POST", requestUrl, true)
-    request.setRequestHeader("Authorization","Basic " + btoa(unescape(encodeURIComponent(accountSID + ":" + authToken))))
-    request.setRequestHeader("Content-type","application/x-www-form-urlencoded")
+    request.open('POST', requestUrl, true)
+    request.setRequestHeader('Authorization', 'Basic ' + btoa(unescape(encodeURIComponent(accountSID + ':' + authToken))))
+    request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded')
 
     request.onreadystatechange = function () {
 
@@ -138,7 +139,7 @@ function dial() {
 
         if (!responseText) {
 
-          updateStatusField("undefined error")
+          updateStatusField('undefined error')
 
         } else {
 
@@ -156,38 +157,39 @@ function dial() {
 
     updateStatusField(error)
 
-  }  
+  }
 }
 
-function onLoad() {
+function onLoad () {
 
   fromNumberField.innerHTML = fromNumber
 
   toNumberField.innerHTML = toNumber
 
-  if (displayNumber === 0) { toNumberField.innerHTML = "**********"; }
+  if (displayNumber === 0) { toNumberField.innerHTML = '**********'; }
 
   var responseText = getMetaData()
 
-  if (!responseText) { setDefault()
+  if (!responseText) {
+    setDefault()
     return false
   }
 
   processResponse(responseText)
 }
 
-function setDefault() {
+function setDefault () {
 
   dialBtn.disabled = false
 
   updateStatusBtn.disabled = true
 
-  sidField.innerHTML = ""
+  sidField.innerHTML = ''
 
-  updateStatusField("Ready to call")
+  updateStatusField('Ready to call')
 }
 
-function processResponse(responseText) {
+function processResponse (responseText) {
 
   dialBtn.disabled = false
 
@@ -201,7 +203,7 @@ function processResponse(responseText) {
 
   } catch (error) {
 
-    updateStatusField("Response JSON not valid")
+    updateStatusField('Response JSON not valid')
 
     return false
   }
@@ -213,8 +215,7 @@ function processResponse(responseText) {
     return false
   }
 
-  if ( dialJSON.status && dialJSON.uri && dialJSON.sid && dialJSON.subresource_uris.recordings ) 
-  {
+  if (dialJSON.status && dialJSON.uri && dialJSON.sid && dialJSON.subresource_uris.recordings) {
 
     dialBtn.disabled = true
 
@@ -224,25 +225,25 @@ function processResponse(responseText) {
 
     updateStatusField(dialJSON.status)
 
-    var s = dialJSON.status + "|" + dialJSON.sid + "|" + rootUrl + dialJSON.uri + "|" + rootUrl + dialJSON.subresource_uris.recordings
+    var s = dialJSON.status + '|' + dialJSON.sid + '|' + rootUrl + dialJSON.uri + '|' + rootUrl + dialJSON.subresource_uris.recordings
 
     setAnswer(s)
 
   } else {
 
-    updateStatusField("Response not valid")
+    updateStatusField('Response not valid')
 
-    setAnswer("Response not valid")
+    setAnswer('Response not valid')
   }
 }
 
 //global function
-function clearAnswer() {
+function clearAnswer () {
 
   //global
-  setAnswer("")
+  setAnswer('')
 
-  setMetaData("")
+  setMetaData('')
 
   //local
   setDefault()
